1a224823d3e19f6fa910fb8061b3e740c6ab8cda
Kumar, Pramod <pramod_kumar@homedepot.com>
2018-01-11 15:42:11 -0500

    remove line

--- src/main/java/com/hd/EurekaServerApplication.java
+++ src/main/java/com/hd/EurekaServerApplication.java
@@ -1,7 +1,7 @@
 package com.hd;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
